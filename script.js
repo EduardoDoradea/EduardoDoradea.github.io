ae9920de273785a049e588af59532b4306013ab9
EduardoDoradea <00086224@uca.edu.sv>
2026-02-27 09:26:02 -0600

    Actualizando esqueleto y logica

--- script.js
+++ script.js
@@ -3,9 +3,10 @@ function calcularPorcentaje() {
     const monto = parseFloat(document.getElementById('monto').value);
     const porcentajeInteres = parseFloat(document.getElementById('porcentaje').value);
     const meses = parseInt(document.getElementById('meses').value);
+    const seguro = parseFloat(document.getElementById('seguro').value); 
 
     // Validar entradas
-    if (isNaN(monto) || isNaN(porcentajeInteres) || isNaN(meses) || monto <= 0 || porcentajeInteres <= 0 || meses <= 0) {
+    if (isNaN(monto) || isNaN(porcentajeInteres) || isNaN(meses) || isNaN(seguro) || monto <= 0 || porcentajeInteres <= 0 || meses <= 0 || seguro < 0) {
         alert("Por favor, ingrese valores válidos mayores a 0 en todos los campos.");
         return;
     }
@@ -20,15 +21,20 @@ function calcularPorcentaje() {
 
     const cuotaMensual = (pagoFinal / meses);
 
+    const cuotaMensualConSeguro = cuotaMensual + seguro;
+
+
     // Mostrar resultados en la interfaz
 
     document.getElementById('resumen-monto').innerText = '$' + monto.toLocaleString('es-ES');
     document.getElementById('resumen-porcentaje').innerText = porcentajeInteres + '%';
     document.getElementById('resumen-meses').innerText = meses + ' meses';
+    document.getElementById('resumen-seguro').innerText = '$' + seguro.toLocaleString('es-ES');
 
     document.getElementById('cuotaMensual').innerText = '$' + cuotaMensual.toLocaleString('es-ES', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
     document.getElementById('totalInteres').innerText = '$' + pagoInteres.toLocaleString('es-ES', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
     document.getElementById('pagoFinal').innerText = '$' + pagoFinal.toLocaleString('es-ES', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+    document.getElementById('cuotaMensualConSeguro').innerText = '$' + cuotaMensualConSeguro.toLocaleString('es-ES', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
 
     // AGREGAMOS LA CLASE PARA ACTIVAR LA ANIMACIÓN FADE-IN
     document.getElementById('bloque-resumen').classList.add('mostrar');
@@ -38,6 +44,7 @@ function limpiarCampos() {
     document.getElementById('monto').value = '';
     document.getElementById('porcentaje').value = '';
     document.getElementById('meses').value = '';
+    document.getElementById('seguro').value = '';
     // QUITAMOS LA CLASE PARA QUE DESAPAREZCA SUAVEMENTE
     document.getElementById('bloque-resumen').classList.remove('mostrar');
 }
